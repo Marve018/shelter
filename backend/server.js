cbd388708faf631a095fc0cad113ebef69263fef
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-07-07 23:38:49 +0100

    chore: Update server.js to connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Welcome to Shelter!');
