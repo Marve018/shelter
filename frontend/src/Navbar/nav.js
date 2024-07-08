f84347f6ae8d6195753f3a5d2f6ccc72bf678c50
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-07-08 05:36:39 +0100

    created navbar

--- frontend/src/Navbar/nav.js
+++ frontend/src/Navbar/nav.js
@@ -1,13 +1,25 @@
 import React from "react";
+import logo from "../Images/logo-img.jpeg";
 
 export default function Nav() {
   return (
     <nav>
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
-          Shelter
+        <a href={logo} className="brand-logo">
+          <img src={logo} alt="logo" />
         </a>
-        <ul></ul>
+        <h1>YOUR PLACE OF SHELTER</h1>
+        <div className="nav-links">
+          <li>
+            <a href="/LOGIN">LOGIN</a>
+          </li>
+          <li>
+            <a href="/SIGNUP">SIGN-UP</a>
+          </li>
+          <li>
+            <a href="/VIEW">VIEW PROPERTY</a>
+          </li>
+        </div>
       </div>
     </nav>
   );
