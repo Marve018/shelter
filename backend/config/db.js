c11dd4778ab07760ebc6b55ae2da089bee493185
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-07-12 00:27:10 +0100

    chore: Update MongoDB connection host in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
