e5f26911bbdeb1c287fb89953a15b067b6e482fa
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-07-13 14:19:46 +0100

    chore: Add lazy loading for property images and update home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Nav from "./Navbar/nav";
 import RegisterForm from "./Forms/RegisterForm";
 import Home from "./Property/home";
+import properties from "./data";
 
 function App() {
+  const house = properties.map((item) => {
+    return <Home item={item} key={item.id} />;
+  });
   return (
     <div className="App">
       <Nav />
       <RegisterForm />
-      <Home />
+      {house}
     </div>
   );
 }
--- frontend/src/Property/home.js
+++ frontend/src/Property/home.js
@@ -2,16 +2,24 @@ import React from "react";
 import "./home.css";
 
 export default function Home(props) {
+  // const imageSrc = require(`../Images/${props.item.image}`).default;
+  const imageSrc = (
+    <img
+      src={`../images/${props.item.image}`}
+      alt="Product"
+      className="property-image"
+    />
+  );
+  console.log(imageSrc);
   return (
     <>
       <div className="property-container">
         <div className="property-card">
-          <img src="" alt="Product" className="property-image" />
-          <h2 className="property-name">ghh</h2>
-          <p className="property-amount">gvhv</p>
-          <p className="property-text">
-            This will be a brief description of the property
-          </p>
+          <img src={imageSrc} alt="Product" className="property-image" />
+          <h2 className="property-name">{props.item.name}</h2>
+          <p className="property-amount">{props.item.price}</p>
+          <p className="property-text">{props.item.description}</p>
+          <p>{props.item.location}</p>
         </div>
       </div>
     </>
