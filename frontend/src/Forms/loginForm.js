48a1e45d512dac1364b79452d6e4a1977b7ec333
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-07-16 05:10:00 +0100

    chore: Update login and register forms

--- frontend/src/Forms/loginForm.js
+++ frontend/src/Forms/loginForm.js
@@ -3,6 +3,12 @@ import "./RegisterForm.css";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleShowLoginForm = () => {
+    setShowLoginForm(!showLoginForm);
+    console.log("Show login form");
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -14,38 +20,57 @@ const LoginForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("login successful");
   };
 
   return (
     <>
-      <div className="form-container">
-        <form onSubmit={handleSubmit} className="form">
-          <div>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              className="form--input"
+      {showLoginForm && (
+        <div className="form-container">
+          <form onSubmit={handleSubmit} className="form">
+            <img
+              src={`${process.env.PUBLIC_URL}/Images/logo-img.jpeg`}
+              alt="logo"
+              className="shelter-logo"
             />
-          </div>
-          <div>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleChange}
-              className="form--input"
-            />
-          </div>
-          <button type="submit" className="form--submit">
-            Login
-          </button>
-        </form>
-      </div>
+            <label className="form--label">Login</label>
+            <div>
+              <div onClick={handleShowLoginForm} className="close-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 50 50"
+                >
+                  <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
+                </svg>
+              </div>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                className="form--input"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                className="form--input"
+              />
+            </div>
+            <button type="submit" className="form--submit">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
