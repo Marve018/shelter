4b29b7a924eac862e7ea48a85ece7c5c97843065
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-07-18 23:39:06 +0100

    chore: Update MongoDB connection logic to handle reconnections

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,15 +4,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+    if (mongoose.connection.readyState === 0) { // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+        try {
+            await mongoose.connect(process.env.MONGO_URI);
+            console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        }
     }
 };
 
