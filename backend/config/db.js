87ed8dc566bf6ffb85e123d2e8d809e57bacd070
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-08-08 16:39:22 +0100

    chore: Update mongo config

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,13 +6,18 @@ dotenv.config();
 const connectDB = async () => {
     if (mongoose.connection.readyState === 0) { // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
         try {
-            await mongoose.connect(process.env.MONGO_URI);
+            await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                ssl: true
+            });
             console.log(`MongoDB Connected: ${mongoose.connection.host}`);
         } catch (error) {
             console.error(`Error: ${error.message}`);
             process.exit(1);
         }
     }
+
 };
 
 module.exports = connectDB;
