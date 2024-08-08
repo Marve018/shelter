67d9ffa4ec35e2d90209fdecd410c8b2e7370bf6
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-08-08 17:53:30 +0100

    chore: Update mongo config

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
             await mongoose.connect(process.env.MONGO_URI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
-                ssl: true
             });
             console.log(`MongoDB Connected: ${mongoose.connection.host}`);
         } catch (error) {
